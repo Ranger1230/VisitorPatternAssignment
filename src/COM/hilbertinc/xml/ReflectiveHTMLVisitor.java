6c1990084b63a67dcf50825e3ab15c3398d49622
aaronmyers <aaronmyers@10.20.31.74>
2014-12-07 15:18:51 -0600

    visitor outputs html with node identification and value

--- src/COM/hilbertinc/xml/ReflectiveHTMLVisitor.java
+++ src/COM/hilbertinc/xml/ReflectiveHTMLVisitor.java
@@ -11,17 +11,17 @@ public class ReflectiveHTMLVisitor extends AbstractReflectiveVisitor {
 	}
 	
 	public boolean visitDocument(Document document){
-		System.out.print("Document");
+		System.out.print("<h1>Document name: " + document.getNodeName() + ", Document value: " + document.getNodeValue() + "<h1>\n");
 		return true;
 	}
 	
 	public boolean visitElement(Element element){
-		System.out.print("element");
+		System.out.print("<h2>Element name: " + element.getNodeName() + ", Element value: " + element.getNodeValue() + "<h2>\n");
 		return true;
 	}
 	
 	public boolean visitText(Text text){
-		System.out.print("text");
+		System.out.print("<h3>Text name: " + text.getNodeName() + ", Text value: " + text.getNodeValue() + "<h3>\n");
 		return true;
 	}
 
